41a1a63d466e71cd722a66653344405ec26f8ad6
Oleh Ivsv <o.ivasiv@abtollc.com>
2023-12-14 20:48:21 +0200

    Add common project for messaging contracts

---
+++ src/Contracts/CatalogItem/CatalogItemUpdated.cs
@@ -0,0 +1,19 @@
+namespace Contracts.CatalogItem;
+
+public class CatalogItemUpdated
+{
+    public Guid Id { get; set; }
+    public string Title { get; init; }
+    public int OriginalPrice { get; init; }
+    public int SoldAmount { get; init; }
+    public string Seller { get; init; }
+    public Guid FloraId { get; init; }
+    public string Status { get; init; }
+    public DateTime OfferEndsAt { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdatedAt { get; init; }
+    public string Name { get; init; }
+    public string? Color { get; init; }
+    public DateTime? LifeTime { get; init; }
+    public List<string> Pictures { get; init; }
+}
---
+++ src/SearchService/Consumers/CatalogItemCreatedConsumer.cs
@@ -0,0 +1,25 @@
+using AutoMapper;
+using Contracts.CatalogItem;
+using MassTransit;
+using SearchService.Data;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class CatalogItemCreatedConsumer : IConsumer<CatalogItemCreated>
+{
+    private readonly IMapper _mapper;
+    private readonly IItemRepository _itemRepository;
+
+    public CatalogItemCreatedConsumer(IMapper mapper, IItemRepository itemRepository)
+    {
+        _mapper = mapper;
+        _itemRepository = itemRepository;
+    }
+    
+    public async Task Consume(ConsumeContext<CatalogItemCreated> context)
+    {
+        var item = _mapper.Map<Item>(context.Message);
+        await _itemRepository.Insert(item);
+    }
+}
