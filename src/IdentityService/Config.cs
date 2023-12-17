8958faecc175fee84fa680fa482b6db30933411f
Oleh Ivsv <o.ivasiv@abtollc.com>
2023-12-17 14:09:36 +0200

    Add basic identity server with OpenId Connect and OAuth2.0 basic implementation

---
+++ src/IdentityService/Config.cs
@@ -0,0 +1,33 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityService;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new("purplelib-app", "PurplerLib app full access"),
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new()
+            {
+                ClientId = "jetbrains",
+                ClientName = "jetbrains",
+                AllowedScopes = { "purplelib-app", "openid", "profile" },
+                RedirectUris = { "https://youtube.com" },
+                ClientSecrets = { new("jetbrains_secret".Sha256()) },
+                AllowedGrantTypes = { GrantType.ResourceOwnerPassword }
+            }
+        };  
+}
---
+++ src/IdentityService/Data/SeedData.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Claims;
+using IdentityModel;
+using IdentityService.Data;
+using IdentityService.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace IdentityService;
+
+public static class SeedData
+{
+    public static void  EnsureSeedData(WebApplication app)
+    {
+        using var scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+        var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+        context.Database.Migrate();
+
+        var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+        if (userMgr.Users.Any())
+            return;
+
+        var alice = new ApplicationUser
+        {
+            UserName = "alice",
+            Email = "AliceSmith@email.com",
+            EmailConfirmed = true,
+        };
+        var aliceResult = userMgr.CreateAsync(alice, "Pass123$").Result;
+
+        if (!aliceResult.Succeeded)
+            throw new Exception(aliceResult.Errors.First().Description);
+
+        aliceResult = userMgr.AddClaimsAsync(alice,
+            new Claim[]
+            {
+                new(JwtClaimTypes.Name, "Alice Smith"),
+            }).Result;
+
+        if (!aliceResult.Succeeded)
+            throw new Exception(aliceResult.Errors.First().Description);
+
+        Log.Debug("alice created");
+
+
+        var bob = new ApplicationUser
+        {
+            UserName = "bob",
+            Email = "BobSmith@email.com",
+            EmailConfirmed = true
+        };
+        var bobResult = userMgr.CreateAsync(bob, "Pass123$").Result;
+
+        if (!bobResult.Succeeded)
+            throw new Exception(bobResult.Errors.First().Description);
+
+        bobResult = userMgr.AddClaimsAsync(bob,
+            new Claim[]
+            {
+                new(JwtClaimTypes.Name, "Bob Smith"),
+            }).Result;
+
+        if (!bobResult.Succeeded)
+            throw new Exception(bobResult.Errors.First().Description);
+
+        Log.Debug("bob created");
+    }
+}
